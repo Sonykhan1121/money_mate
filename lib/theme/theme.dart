4ef016090742e6b22bc7f7fb641cc9ba613947b1
sonykhan1121@gmail.com <sonykhan1121@gmail.com>
2025-10-22 17:16:17 +0600

    add colors

---
+++ lib/theme/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DAppTheme {
+  DAppTheme._();
+
+  // light theme
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+
+  );
+}
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class DColors{
+  DColors._();
+
+  static const Color primary = Color(0xff00897b);
+  static const Color secondary = Color(0xffe3f3e3);
+  static const Color primarySecondary = Color(0xff007466);
+  static const Color grey = Color(0xff757575);
+  static const Color success = Color(0xff4caf50);
+  static const Color error = Color(0xfff44336);
+  static const Color fBlack = Color(0xff212121);
+  static const Color fWhite = Color(0xffffffff);
+
+}
