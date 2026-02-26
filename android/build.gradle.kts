015b0e4f5b60ea0b67bc5d96274f5d7592990eba
sonykhan1121@gmail.com <sonykhan1121@gmail.com>
2026-02-26 10:15:31 +0600

    downgrade AGP

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,3 +19,13 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    afterEvaluate {
+        if (extensions.findByName("android") != null) {
+            extensions.getByType(com.android.build.gradle.BaseExtension::class).apply {
+                compileSdkVersion(36)
+            }
+        }
+    }
+}
